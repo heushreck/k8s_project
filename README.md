7b0386b192b4063f49fcac0b2674b658d68aec2d
Nicolas Neudeck <nicolas.neudeck@outlook.de>
2022-03-03 23:16:51 +0100

    add real readme and cleaned app.py

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# Kubernetes Flask Project
-test123
+# Postgres Flask Docker Project
+Basic Python Flask app in Docker which accesses a postgresql database in an other docker container
+
+## Usage
+### Build & Run application
+Build the Docker image manually by cloning the Git repo.
+```
+$ git clone <link>
+$ cd <foldername>
+$ docker-compose up
+```
+### Verify the running app
+visit http://localhost:1234
+```
+ Sample Docker Flask App
+```
+Response should be html with the first row of a table in the postgresql database
+
+visit http://localhost:1234/api/v1/all/events?id=10 
+
+to receive json response of event with id 10
+
+## Explaination
+Short explainaition of both docker containers
+
+### App
+Flask app, using the python:3.8-slim-buster image and installing python packages listed in `requirements.txt`.
+
+The sqlalchemy package connect to the postgresql database.
+
+I production, the database username, password etc would be environment variables handled by Secrets
+
+### Database
+from the latest `postgres` image a container is started. 
+
+The `initial_setup.sql` file is copied into the `/docker-entrypoint-initdb.d` folder to initialize the database on first start.
+
+### Network
+the two seperate docker containers get build and started via the `docker-compose.yml` file in root.
+
+A network acts as a bridge between the containers.
+
+Post 1234:1234 is exposed
+
--- app/app.py
+++ app/app.py
@@ -19,24 +19,17 @@ app.config["DEBUG"] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = database_uri
 
 
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
-
-
 @app.route('/')
 def index():
     with engine.connect() as eng:
         rs = eng.execute(
-            f"SELECT * FROM american_football_action_participants LIMIT 10")
+            f"SELECT * FROM american_football_action_participants LIMIT 1")
         num = ""
         for row in rs:
             print(row)
             num = row
         # eng.commit()
-    return f'<h1>Distant Reading Archive</h1><p>A prototype API for distant reading of science fiction novels, selected {num} rows</p>'
+    return f'<h1>Sample Docker Flask App</h1><p>Here is the first row of the american_football_action_participants Table: {num}</p>'
 
 
 @app.route('/api/v1/all/events', methods=['GET'])
@@ -55,48 +48,6 @@ def api_event_by_id():
 
     return jsonify(player)
 
-
-""" 
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return "<h1>404</h1><p>The resource could not be found.</p>", 404
-
-
-@app.route('/api/v1/resources/books', methods=['GET'])
-def api_filter():
-    query_parameters = request.args
-
-    id = query_parameters.get('id')
-    published = query_parameters.get('published')
-    author = query_parameters.get('author')
-
-    query = "SELECT * FROM books WHERE"
-    to_filter = []
-
-    if id:
-        query += ' id=? AND'
-        to_filter.append(id)
-    if published:
-        query += ' published=? AND'
-        to_filter.append(published)
-    if author:
-        query += ' author=? AND'
-        to_filter.append(author)
-    if not (id or published or author):
-        return page_not_found(404)
-
-    query = query[:-4] + ';'
-
-    conn = sqlite3.connect('books.db')
-    conn.row_factory = dict_factory
-    cur = conn.cursor()
-
-    results = cur.execute(query, to_filter).fetchall()
-
-    return jsonify(results)
- """
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=1234)
